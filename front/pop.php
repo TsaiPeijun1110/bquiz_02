5e98bed9c2f361e002020af83dacfa1ef7cc51c0
tsai <lovez81427@gmail.com>
2024-01-08 11:28:47 +0800

    完成人氣文章彈出視窗功能 + 彈出視窗增加文章標題

--- front/pop.php
+++ front/pop.php
@@ -6,39 +6,56 @@
             <th width="50%">內容</th>
             <th>人氣</th>
         </tr>
-        <?php 
-        $total=$News->count(['sh'=>1]);
-        $div=5;
-        $pages=ceil($total/$div);
-        $now=$_GET['p']??1;
-        $start=($now-1)*$div;
+        <?php
+        $total = $News->count(['sh' => 1]);
+        $div = 5;
+        $pages = ceil($total / $div);
+        $now = $_GET['p'] ?? 1;
+        $start = ($now - 1) * $div;
 
-        $rows=$News->all(['sh'=>1]," order by `good` desc limit $start,$div");
-        foreach($rows as $row){
+        $rows = $News->all(['sh' => 1], " order by `good` desc limit $start,$div");
+        foreach ($rows as $row) {
         ?>
-        <tr>
-            <td><?=$row['title'];?></td>
-            <td><?=mb_substr($row['news'],0,25);?>...</td>
-            <td></td>
-        </tr>
+            <tr>
+                <td>
+                    <div class='title' data-id="<?= $row['id']; ?>"><?= $row['title']; ?></div>
+                </td>
+                <td style="position: relative;">
+                    <div><?= mb_substr($row['news'], 0, 25); ?></div>
+                    <div id="p<?= $row['id']; ?>" class="pop">
+                    <h3 style='color: skyblue'><?=$row['title'];?></h3>
+                        <pre><?= $row['news']; ?></pre>
+                    </div>
+                </td>
+                <td></td>
+            </tr>
         <?php
         }
         ?>
     </table>
     <div>
-    <?php
-    if(($now-1)>0){
-        $prev=$now-1;
-        echo "<a href='?do=pop&p=$prev'> < </a>";
-    }
-    for($i=1;$i<=$pages;$i++){
-        $fontsize=($now==$i)?"font-size:20px":"font-size:16px";
-        echo "<a href='?do=pop&p=$i' style='$fontsize'> $i </a>";
-    }
-    if(($now+1)<=$pages){
-        $next=$now+1;
-        echo "<a href='?do=pop&p=$next'> > </a>";
-    }
-    ?>
-    </div>    
+        <?php
+        if (($now - 1) > 0) {
+            $prev = $now - 1;
+            echo "<a href='?do=pop&p=$prev'> < </a>";
+        }
+        for ($i = 1; $i <= $pages; $i++) {
+            $fontsize = ($now == $i) ? "font-size:20px" : "font-size:16px";
+            echo "<a href='?do=pop&p=$i' style='$fontsize'> $i </a>";
+        }
+        if (($now + 1) <= $pages) {
+            $next = $now + 1;
+            echo "<a href='?do=pop&p=$next'> > </a>";
+        }
+        ?>
+    </div>
 </fieldset>
+<script>
+    $(".title").hover(
+        function() {
+            $(".pop").hide()
+            let id = $(this).data("id")
+            $("#p" + id).show();
+        }
+    )
+</script>
